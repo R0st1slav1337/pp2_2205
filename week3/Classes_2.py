f83558364a06454aeba486b31e42bcf0e181959e
Rostislav <r_krivenko@kbtu.kz>
2025-02-01 17:35:57 +0500

    Add Triangle class and area calculation

--- week3/Classes_2.py
+++ week3/Classes_2.py
@@ -13,5 +13,16 @@ class Square(Shape): # Child class Square inheriting from Shape class
     def area(self): # method to calculate area of square
         return self.side_length ** 2
 
-square = Square(int(input()))
+class Triangle(Shape): # Child class Triangle inheriting from Shape class
+    def __init__(self, length, width):
+        super().__init__(3)
+        self.length = length
+        self.width = width
+
+    def area(self): # method to calculate area of triangle
+        return (self.length * self.width)/2
+
+square = Square(int(input("Enter side length of square: ")))
 print(square.area())
+triangle = Triangle(int(input("Enter length of triangle: ")), int(input("Enter width of triangle: ")))
+print(triangle.area())
