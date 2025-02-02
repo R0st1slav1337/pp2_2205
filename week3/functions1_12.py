cdf63c20d118cd6947125f0edf9f6b119ff9810c
Rostislav <r_krivenko@kbtu.kz>
2025-02-02 21:00:43 +0500

    upd

---
+++ week3/functions1_12.py
@@ -0,0 +1,5 @@
+def histogram(lst):
+    for num in lst: # Loop through the list of numbers
+        print('*' * num) # Print the number of asterisks based on the current number
+
+histogram([4, 9, 7])
