ec656ef29a88473b31f9c0c1e117ef121d7deda7
Rostislav <r_krivenko@kbtu.kz>
2025-02-03 17:28:25 +0500

    Add function to filter movies by category and display related movies

--- week3/functions2_1.py
+++ week3/functions2_1.py
@@ -83,8 +83,16 @@ def is_high_rated(movie):
 def high_rated_movies(movies):
     return [movie for movie in movies if movie.get('imdb', 0) > 5.5] # return a list of movies with a rating greater than 5.5
 
+def movies_by_category(movies, category):
+    return [movie for movie in movies if movie.get('category') == category] # return a list of movies with the specified category
+
 print(is_high_rated(movies[1])) # accessing the second movie in the list
 print(is_high_rated(movies[9])) # accessing the tenth movie in the list
 
 print("High-rated movies with IMDB score more than 5.5:")
 print(high_rated_movies(movies)) # get the list of high-rated movies
+
+category_name = str(input("Enter the category to see related movies: ")) # get the category name from the user
+filtered_movies = movies_by_category(movies, category_name) # get the list of movies with the specified category
+for movie in filtered_movies:
+    print(f"Name: {movie['name']}, IMDB: {movie['imdb']}") # print the name and IMDB score of each movie with the specified category
